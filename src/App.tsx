73bc07ec084684ebc14d5d8ae972c47156868cc9
Aleksey <aliaksei.balabushka@gmail.com>
2023-03-14 11:04:46 +0300

    feat: add eslint and prettier in project

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import React from 'react';
+
 function App() {
   return (
     <div>
-      <h1>Hello world, I'm Vite</h1>
+      <h1>Hello world</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
