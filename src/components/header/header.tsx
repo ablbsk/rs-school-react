404c8107ae889f0182dc265788d8504ffda899de
Aleksey <aliaksei.balabushka@gmail.com>
2023-03-16 19:15:18 +0300

    fix: fix card list bottom padding and active link in header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,8 +1,19 @@
 import './header.scss';
 import React, { FunctionComponent } from 'react';
-import { Link } from 'react-router-dom';
+import classNames from 'classnames';
+import { Link, useLocation } from 'react-router-dom';
 
 const Header: FunctionComponent = () => {
+  const location = useLocation();
+  console.log(location.pathname);
+
+//   className={classNames(
+//     'card__tag',
+//   { 'card__tag--female': character.gender === 'Female' },
+//   { 'card__tag--male': character.gender === 'Male' },
+//   { 'card__tag--genderless': character.gender === 'Genderless' }
+// )}
+
   return (
     <header className="header">
       <div className="wrapper header__wrapper">
@@ -12,12 +23,22 @@ const Header: FunctionComponent = () => {
         <nav className="navigation">
           <ul className="navigation__list">
             <li className="navigation__item">
-              <Link className="navigation__link" to="/">
+              <Link
+                className={classNames('navigation__link', {
+                  'navigation__link--active': location.pathname === '/',
+                })}
+                to="/"
+              >
                 Home
               </Link>
             </li>
             <li className="navigation__item">
-              <Link className="navigation__link" to="/about-us">
+              <Link
+                className={classNames('navigation__link', {
+                  'navigation__link--active': location.pathname === '/about-us',
+                })}
+                to="/about-us"
+              >
                 About Us
               </Link>
             </li>
