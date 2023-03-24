1bd86b8f6b5125d26182bf06ff4579a7d76bf0ae
Aleksey <aliaksei.balabushka@gmail.com>
2023-03-24 18:14:43 +0300

    feat: add form validation

--- src/components/form/index.tsx
+++ src/components/form/index.tsx
@@ -1,42 +1,15 @@
 import "./form.scss";
-import React, { Component, Ref } from "react";
+import React, { Component, FormEvent, Ref, RefObject } from "react";
+import classNames from "classnames";
+import { IFeedback, IFeedbackError } from "../../interfaces";
+import validate from "./validation";
 
-class Form extends Component<object, object> {
+class Form extends Component<object, IFeedback> {
   private readonly formRef: Ref<HTMLFormElement>;
-
-  private readonly usernameInput: Ref<HTMLInputElement>;
-
-  private readonly continentsSelect: Ref<HTMLSelectElement>;
-
-  private readonly emailInput: Ref<HTMLInputElement>;
-
-  private readonly dateInput: Ref<HTMLInputElement>;
-
-  private readonly fileInput: Ref<HTMLInputElement>;
-
-  private readonly ratingGood: Ref<HTMLInputElement>;
-
-  private readonly ratingMaybe: Ref<HTMLInputElement>;
-
-  private readonly ratingBad: Ref<HTMLInputElement>;
-
-  private readonly opinionTextarea: Ref<HTMLTextAreaElement>;
-
-  private readonly confirmCheckbox: Ref<HTMLInputElement>;
 
   constructor(props: object) {
     super(props);
     this.formRef = React.createRef();
-    this.usernameInput = React.createRef();
-    this.continentsSelect = React.createRef();
-    this.emailInput = React.createRef();
-    this.dateInput = React.createRef();
-    this.fileInput = React.createRef();
-    this.ratingGood = React.createRef();
-    this.ratingMaybe = React.createRef();
-    this.ratingBad = React.createRef();
-    this.opinionTextarea = React.createRef();
-    this.confirmCheckbox = React.createRef();
 
     this.state = {
       data: {
@@ -49,29 +22,66 @@ class Form extends Component<object, object> {
         opinion: null,
         isConfirm: false,
       },
-      errors: null,
-      isButtonDisabled: true,
+      errors: {
+        username: null,
+        continents: null,
+        email: null,
+        dateOfBirth: null,
+        picture: null,
+        rating: null,
+        opinion: null,
+        isConfirm: null,
+      },
     };
   }
 
-  handleSubmit(e: Event) {
+  handleSubmit(e: FormEvent) {
     e.preventDefault();
-    // this.setState({
-    //   data: {
-    //     username: this.usernameInput.current.value,
-    //     continents: this.continentsSelect.current.value,
-    //     email: this.emailInput.current.value,
-    //     dateOfBirth: this.dateInput.current.value,
-    //     picture: this.fileInput.current.value,
-    //     rating: this.ratingGood.current.value || this.ratingMaybe.current.value || this.ratingBad.current.value,
-    //     opinion: this.opinionTextarea.current.value,
-    //     isConfirm: this.confirmCheckbox.current.value,
-    //   },
-    // });
-    console.log(this.formRef);
+    if (this.formRef) {
+      const { current } = this.formRef;
+
+      this.setState(
+        {
+          data: {
+            username: current[0].value,
+            continents: current[1].value,
+            email: current[2].value,
+            dateOfBirth: current[3].value,
+            picture: current[4].value,
+            // eslint-disable-next-line no-nested-ternary
+            rating: current[5].checked ? "good" : current[6].checked ? "maybe" : "bad",
+            opinion: current[8].value,
+            isConfirm: current[9].checked,
+          },
+        },
+        this.validateFields
+      );
+    }
+  }
+
+  validateFields() {
+    const { data } = this.state;
+    const errors = validate(data);
+
+    this.setState({ data, errors });
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  createError(error: string | null) {
+    return (
+      <span
+        className={classNames("form__error", {
+          "form__error--active": error,
+        })}
+      >
+        {error}
+      </span>
+    );
   }
 
   render() {
+    const { errors } = this.state;
+
     return (
       <div className="form">
         <h3 className="form__header">Send us your feedback!</h3>
@@ -82,21 +92,23 @@ class Form extends Component<object, object> {
                 <label className="form__label" htmlFor="username">
                   Username
                   <input
-                    ref={this.usernameInput}
                     id="username"
                     className="form__input form__item"
                     type="text"
                     placeholder="Morty"
                   />
-                  <span className="form__error">* Wrong value</span>
+                  {this.createError(errors.username)}
                 </label>
                 <label className="form__label form__label--short" htmlFor="continents">
                   Continents
                   <select
-                    ref={this.continentsSelect}
                     id="continents"
                     className="form__dropdown form__item form__item--short"
+                    defaultValue="default"
                   >
+                    <option value="default" disabled>
+                      Select
+                    </option>
                     <option value="africa">Africa</option>
                     <option value="asia">Asia</option>
                     <option value="europe">Europe</option>
@@ -105,38 +117,36 @@ class Form extends Component<object, object> {
                     <option value="antarctica">Antarctica</option>
                     <option value="australia">Australia</option>
                   </select>
-                  <span className="form__error">* Wrong value</span>
+                  {this.createError(errors.continents)}
                 </label>
               </div>
               <div className="form__line">
                 <label className="form__label" htmlFor="email">
                   Email
                   <input
-                    ref={this.emailInput}
                     className="form__input form__item"
                     id="email"
                     type="email"
                     placeholder="morty.smith@gmail.com"
                   />
-                  <span className="form__error">* Wrong value</span>
+                  {this.createError(errors.email)}
                 </label>
                 <label className="form__label form__label--short" htmlFor="birth">
                   Date of birth
-                  <input ref={this.dateInput} id="birth" className="form__item" type="date" />
-                  <span className="form__error">* Wrong value</span>
+                  <input id="birth" className="form__item" type="date" />
+                  {this.createError(errors.dateOfBirth)}
                 </label>
               </div>
               <div className="form__line">
                 <label className="form__label form__label--file" htmlFor="avatar">
                   Profile picture
                   <input
-                    ref={this.fileInput}
                     className="form__file form__item"
                     type="file"
                     id="avatar"
                     accept="image/png, image/jpeg, image/jpg"
                   />
-                  <span className="form__error">* Wrong value</span>
+                  {this.createError(errors.picture)}
                 </label>
               </div>
             </div>
@@ -145,33 +155,15 @@ class Form extends Component<object, object> {
                 <span className="form__label">Evaluate the project</span>
                 <div className="form__radio-container">
                   <label htmlFor="ratingGood">
-                    <input
-                      ref={this.ratingGood}
-                      id="ratingGood"
-                      type="radio"
-                      name="rating"
-                      value="good"
-                    />
+                    <input id="ratingGood" type="radio" name="rating" value="good" defaultChecked />
                     <span className="form__radio-icon icon--smile-good" />
                   </label>
                   <label htmlFor="ratingMaybe">
-                    <input
-                      ref={this.ratingMaybe}
-                      id="ratingMaybe"
-                      type="radio"
-                      name="rating"
-                      value="maybe"
-                    />
+                    <input id="ratingMaybe" type="radio" name="rating" value="maybe" />
                     <span className="form__radio-icon icon--smile-maybe" />
                   </label>
                   <label htmlFor="ratingBad">
-                    <input
-                      ref={this.ratingBad}
-                      id="ratingBad"
-                      type="radio"
-                      name="rating"
-                      value="bad"
-                    />
+                    <input id="ratingBad" type="radio" name="rating" value="bad" />
                     <span className="form__radio-icon icon--smile-bad" />
                   </label>
                 </div>
@@ -181,31 +173,24 @@ class Form extends Component<object, object> {
                 <label className="form__label" htmlFor="description">
                   Describe your opinion
                   <textarea
-                    ref={this.opinionTextarea}
                     id="description"
                     className="form__textarea form__item"
                     placeholder="What do you think about this animated series?"
                   />
-                  <span className="form__error">* Wrong value</span>
+                  {this.createError(errors.opinion)}
                 </label>
               </div>
             </div>
             <div className="form__section form__section--wide">
               <label className="form__check" htmlFor="checkbox">
-                <input
-                  ref={this.confirmCheckbox}
-                  className="form__checkbox"
-                  type="checkbox"
-                  id="checkbox"
-                  name="checkbox"
-                />
+                <input className="form__checkbox" type="checkbox" id="checkbox" name="checkbox" />
                 agree to send my data
               </label>
-              <span className="form__error">* Wrong value</span>
+              {this.createError(errors.isConfirm)}
               <button
                 className="button form__button"
                 type="submit"
-                onClick={(e: Event) => this.handleSubmit(e)}
+                onClick={(e: FormEvent) => this.handleSubmit(e)}
               >
                 Create feedback
               </button>
--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -29,13 +29,29 @@ export interface ICharacter {
   created: string;
 }
 
-// export interface IFeedback {
-//   username: string | null;
-//   continents: string | null;
-//   email: string | null;
-//   dateOfBirth: Date | null;
-//   picture: string | null;
-//   rating: string | null;
-//   opinion: string | null;
-//   isConfirm: boolean;
-// }
+export interface IFeedback {
+  data: IFeedbackFields;
+  errors: IFeedbackErrors;
+}
+
+export interface IFeedbackFields {
+  username: string | null;
+  continents: string | null;
+  email: string | null;
+  dateOfBirth: any | null; // TODO Change type of filed
+  picture: string | null;
+  rating: string | null;
+  opinion: string | null;
+  isConfirm: boolean;
+}
+
+export interface IFeedbackErrors {
+  username: string | null;
+  continents: string | null;
+  email: string | null;
+  dateOfBirth: string | null;
+  picture: string | null;
+  rating: string | null;
+  opinion: string | null;
+  isConfirm: string | null;
+}
