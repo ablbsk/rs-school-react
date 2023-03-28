2d4c58cf2bd7668b6d7049e43bd93c4c2855b7b7
Aleksey <aliaksei.balabushka@gmail.com>
2023-03-28 11:44:55 +0300

    feat: add home component test

--- src/pages/home/home.test.tsx
+++ src/pages/home/home.test.tsx
@@ -3,10 +3,13 @@ import { render, screen } from "@testing-library/react";
 import React from "react";
 import Home from "./index";
 
-describe("Home component", () => {
-  beforeEach(() => render(<Home />));
-
-  it("search should have placeholder", () => {
-    expect(screen.getByPlaceholderText("What do you want to find?")).toBeDefined();
+describe("Home component", async () => {
+  it("should have search", async () => {
+    const { container } = render(<Home />);
+    expect(container.getElementsByClassName("search").length).toBe(1);
+  });
+  it("should have search button", async () => {
+    render(<Home />);
+    expect(screen.getByRole("button")).toHaveClass("search__button");
   });
 });
