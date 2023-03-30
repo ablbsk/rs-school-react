41ad608bfc839015a9f1258c6336cf7dca74418c
Aleksey <aliaksei.balabushka@gmail.com>
2023-03-30 09:14:58 +0300

    feat: add test to app component

--- src/components/app/app.test.tsx
+++ src/components/app/app.test.tsx
@@ -4,8 +4,8 @@ import React from "react";
 import { MemoryRouter } from "react-router";
 import App from "./app";
 
-describe("App component", () => {
-  it("Landing on a home page", async () => {
+describe("App component", async () => {
+  it("Should render Home page", async () => {
     render(
       <MemoryRouter initialEntries={["/"]}>
         <App />
@@ -13,5 +13,41 @@ describe("App component", () => {
     );
 
     expect(screen.getByRole("heading", { level: 1 })).toHaveTextContent("The Rick and Morty");
+  });
+
+  it("Should render Not Found page", async () => {
+    const { container } = render(
+      <MemoryRouter initialEntries={["/404"]}>
+        <App />
+      </MemoryRouter>
+    );
+    expect(container.getElementsByClassName("not-found").length).toBe(1);
+  });
+
+  it("Should render Not Found page with wrong route", async () => {
+    const { container } = render(
+      <MemoryRouter initialEntries={["/wrong-rout-example"]}>
+        <App />
+      </MemoryRouter>
+    );
+    expect(container.getElementsByClassName("not-found").length).toBe(1);
+  });
+
+  it("Should render About Us page", async () => {
+    const { container } = render(
+      <MemoryRouter initialEntries={["/about-us"]}>
+        <App />
+      </MemoryRouter>
+    );
+    expect(container.getElementsByClassName("about-us").length).toBe(1);
+  });
+
+  it("Should render Feedback page", async () => {
+    const { container } = render(
+      <MemoryRouter initialEntries={["/feedback"]}>
+        <App />
+      </MemoryRouter>
+    );
+    expect(container.getElementsByClassName("feedback").length).toBe(1);
   });
 });
